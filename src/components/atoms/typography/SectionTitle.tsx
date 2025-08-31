c7706fb2dcaa417d553ba9eaf6839024e2733af6
annastriw <annastriwidagdoo@gmail.com>
2025-09-01 01:31:05 +0700

    feat(ui): adjust SectionTitle font size to smaller scale

--- src/components/atoms/typography/SectionTitle.tsx
+++ src/components/atoms/typography/SectionTitle.tsx
@@ -1,13 +1,26 @@
+"use client";
+
+import { motion } from "framer-motion";
+
 interface SectionTitleProps {
   title: string;
 }
 
 export default function SectionTitle({ title }: SectionTitleProps) {
   return (
-    <div className="text-center">
-      <h1 className="from-primary to-secondary font-paytone inline-block bg-gradient-to-r bg-clip-text text-center text-3xl leading-tight text-transparent md:text-4xl md:leading-snug">
-        {title}
-      </h1>
+    <div className="flex w-full justify-center">
+      <motion.div
+        className="relative inline-block text-center"
+        initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        transition={{ duration: 0.6, ease: "easeOut" }}
+      >
+        {/* Text */}
+        <h1 className="from-primary to-secondary font-paytone inline-block bg-gradient-to-r bg-clip-text text-xl leading-snug text-transparent sm:text-2xl sm:leading-snug md:text-3xl md:leading-snug lg:text-4xl lg:leading-snug">
+          {title}
+        </h1>
+      </motion.div>
     </div>
   );
 }
